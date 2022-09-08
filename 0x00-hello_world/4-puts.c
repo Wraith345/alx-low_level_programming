78398cb12ec79549848f020c4092e159162b3d36
Wraith345 <110994036+Wraith345@users.noreply.github.com>
2022-09-08 21:41:09 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
  * prints task statement 
  * Return: 0
  */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
